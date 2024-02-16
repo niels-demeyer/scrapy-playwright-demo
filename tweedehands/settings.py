92724e155424ba10ef8b9dc1e3e8a483e28f5096
niels demeyer <niels.demeyer99@gmail.com>
2024-02-16 19:08:30 +0100

    added local env

--- tweedehands/settings.py
+++ tweedehands/settings.py
@@ -9,11 +9,12 @@ class ImproperlyConfigured(Exception):
 
 def get_env_variable(var_name):
     """Get the environment variable or return exception."""
+    load_dotenv()  # take environment variables from .env.
     try:
-        return os.environ[var_name]
+        return os.getenv(var_name)
     except KeyError:
-        error_msg = 'Set the {} environment, variable'.format(var_name)
-    raise ImproperlyConfigured(error_msg)
+        error_msg = "Set the {} environment variable in .env file".format(var_name)
+        raise ImproperlyConfigured(error_msg)
 
 
 BOT_NAME = "tweedehands"
@@ -31,90 +32,90 @@ TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 PLAYWRIGHT_PROCESS_REQUEST_HEADERS = None
 
 FEEDS = {
-    './data/data.jsonl': {
-        'format': 'jsonlines',
-        'overwrite': False,
-        'item_filter': GueuzeOnlyFilter,
-        'encoding': 'utf8',
+    "./data/data.jsonl": {
+        "format": "jsonlines",
+        "overwrite": False,
+        "item_filter": GueuzeOnlyFilter,
+        "encoding": "utf8",
     },
 }
 
-TWEEDHANDS_USERNAME=get_env_variable('TWEEDHANDS_USERNAME')
-TWEEDHANDS_PASSWORD=get_env_variable('TWEEDHANDS_PASSWORD')
+TWEEDHANDS_USERNAME = get_env_variable("TWEEDHANDS_USERNAME")
+TWEEDHANDS_PASSWORD = get_env_variable("TWEEDHANDS_PASSWORD")
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "tweedehands (+http://www.yourdomain.com)"
+# USER_AGENT = "tweedehands (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 #    "Accept-Language": "en",
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    "tweedehands.middlewares.TweedehandsSpiderMiddleware": 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    "tweedehands.middlewares.TweedehandsDownloaderMiddleware": 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "tweedehands.pipelines.TweedehandsPipeline": 300,
+    "tweedehands.pipelines.TweedehandsPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = "httpcache"
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
